dd89ddaff0b0204f2a66ab3d6b7a96c44aceef69
SerhiiSlynko <125224953+SerhiiSlynko@users.noreply.github.com>
2025-05-06 17:54:06 +0300

    v

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderComponent title="Мій перший Vue заголовок" />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+/* Можна додати свої стилі */
+</style>
+
+
+
+
+
---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+header {
+  background: #42b983;
+  color: white;
+  padding: 15px;
+  text-align: center;
+}
+</style>
